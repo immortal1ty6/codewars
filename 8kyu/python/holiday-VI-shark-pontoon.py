1d4d53f05978fd88e6adb705b0a85c7b6d3f5577
immortal1ty6 <54611553+immortal1ty6@users.noreply.github.com>
2020-11-13 14:35:08 +0700

    Update solution

--- 8kyu/python/holiday-VI-shark-pontoon.py
+++ 8kyu/python/holiday-VI-shark-pontoon.py
@@ -1,5 +1,8 @@
 def shark(pontoon_distance, shark_distance, you_speed, shark_speed, dolphin):
-    if pontoon_distance / you_speed < shark_distance / shark_speed:
-        return "Alive!"
-    else:
-        return "Shark Bait!"
+    if dolphin:
+        shark_speed = shark_speed / 2
+        
+    shark_eat_time = shark_distance / shark_speed
+    you_safe_time = pontoon_distance / you_speed
+    
+    return "Shark Bait!" if you_safe_time > shark_eat_time else "Alive!"
